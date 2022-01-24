33e69fbd2dd3781079c5645f3c5ed765c7a5c8b2
bruna-gregorio <blgregorio16@gmail.com>
2022-01-24 16:18:39 -0300

    ordenando a listenagem dos cursos

--- src/services/ListCourseService.ts
+++ src/services/ListCourseService.ts
@@ -3,7 +3,11 @@ import prismaClient from "../prisma"
 
 class ListCourseService {
   async execute() {
-    const courses = await prismaClient.course.findMany()
+    const courses = await prismaClient.course.findMany({
+      orderBy: {
+        name: "asc"
+      }
+    })
 
     return courses
   }
