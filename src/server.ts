88744369cf81f5802d66308f935a358f95d4f382
bruna-gregorio <blgregorio16@gmail.com>
2022-02-10 12:07:29 -0300

    adicionando o cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 const express = require('express')
 import { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
+import cors from "cors"
 
 import { routes } from "./routes"
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
